55c8d575b350e6b62b1b3ce8438ecbfae60dfa8a
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2022-06-23 18:03:07 +0200

    recap finito

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="text-center p-5">
+
+    <div>
+      <h2>{{ TitoloPagina }}</h2>
+      <!-- COmponente figlio -->
+      <BtnCambiaTitolo
+        testoBottone="Cliccami per cambiare titolo"
+        @changeTitle="funzioneCambiaTitolo"
+      />
+      <!-- <BtnCambiaTitolo testoBottone="nuovo titolo bottone"/> -->
+    </div>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import BtnCambiaTitolo from './components/BtnCambiaTitolo.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      TitoloPagina: 'Vue Cli Emit'
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
+    BtnCambiaTitolo
+  },
+  methods: {
+    funzioneCambiaTitolo( valoreEmit ){
+      this.TitoloPagina = valoreEmit;
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/BtnCambiaTitolo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <button
+      class="btn btn-success"
+      @click=" $emit( 'changeTitle', 'Vue CLI è molto bello' ) "
+    > -->
+    <button
+      class="btn btn-success"
+      @click="ChangeTitol"
+    >
+      {{ testoBottone }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'BtnCambiaTitolo',
+  props: [ 'testoBottone' ],
+  data(){
+
+  },
+  methods: {
+    ChangeTitol(){
+      this.$emit( 'changeTitle', 'Vue CLI è molto bello' )
+    }
+  }
+}
+
+</script>
